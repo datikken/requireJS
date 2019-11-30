927401527084796fc92916871b380d82834427dc
tikken <big_nick@gmail.com>
2019-11-30 18:18:34 +0300

    initial

---
+++ 6-requirejs-javascript-dependency-injection-m6-exercise-files/After/src/js/main.js
@@ -0,0 +1,11 @@
+require.config({
+    paths: {
+        jquery: 'jquery-2.1.1.min',
+        templates: '../templates',
+        hbs: '../../node_modules/require-handlebars-plugin/hbs'
+    }
+});
+
+require(["app"], function (app) {
+    app.init();
+});
